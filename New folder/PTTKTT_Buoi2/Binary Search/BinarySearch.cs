2e354219cc0c7f57b112d75833690cb953aa546d
kien1282003 <occhoever1231@gmail.com>
2024-07-23 22:56:10 +0700

    update

---
+++ New folder/PTTKTT_Buoi2/Binary Search/BinarySearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search
+{
+    internal class BinarySearch
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            Console.WriteLine("\nNhap so can tim: ");
+            int key = int.Parse(Console.ReadLine());
+            int result = binarySearch(a, key, 11);
+            if (result == -1)
+                Console.WriteLine("Khong tim thay vi tri");
+            else
+                Console.WriteLine("Phan tu duoc tim thay tai vi tri: " + result);
+            Console.ReadKey();
+        }
+        public static int binarySearch(int[] A, int key, int n)
+        {
+            int mid;
+            int left = 0; // vị trí phần tử đầu tiên trong mảng
+            int right = n - 1; // vị trí phần tử cuối cùng trong mảng
+            while (left <= right)
+            {
+                mid = (left + right) / 2; //vị trí giữa mảng
+                if (A[mid] == key)
+                    return mid;
+                if (key < A[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            return -1; // không tìm thấy key trong mảng nên trả về vị trí -1.
+        }
+    }
+}
---
+++ New folder/PTTKTT_Buoi2/Merge Sort/MergeSort.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class Program
+    {
+        public static void MergeSort(int[] array)
+        {
+            if (array.Length <= 1)
+                return;
+
+            int[] left = new int[array.Length / 2];
+            int[] right = new int[array.Length - left.Length];
+
+            Array.Copy(array, 0, left, 0, left.Length);
+            Array.Copy(array, left.Length, right, 0, right.Length);
+
+            MergeSort(left);
+            MergeSort(right);
+            Merge(array, left, right);
+        }
+
+        private static void Merge(int[] result, int[] left, int[] right)
+        {
+            int i = 0, j = 0, k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                    result[k++] = left[i++];
+                else
+                    result[k++] = right[j++];
+            }
+
+            while (i < left.Length)
+                result[k++] = left[i++];
+
+            while (j < right.Length)
+                result[k++] = right[j++];
+        }
+        private static void PrintArray(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 22, 3, 9, 6, 1, 5, 23, 75 };
+            Console.WriteLine("Mang ban dau:");
+            PrintArray(array);
+
+            MergeSort(array);
+
+            Console.WriteLine("\nSap xep:");
+            PrintArray(array);
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
